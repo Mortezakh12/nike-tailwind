2b6fd5e7a380a5cb5aa983dcf199c76a10389722
Mortezakh12 <114151002+Mortezakh12@users.noreply.github.com>
2023-09-02 18:39:30 +0330

    Added ServiceCard component and used it in Services section
    Added SpecialOffer component

--- src/Section/Services.jsx
+++ src/Section/Services.jsx
@@ -1,6 +1,12 @@
+import ServiceCard from "../Components/ServiceCard";
+import { services } from "./../Constants/index";
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
-export default Services
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((services) => {
+        return <ServiceCard key={services.label} {...services} />;
+      })}
+    </section>
+  );
+};
+export default Services;
